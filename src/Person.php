dfaa65fe1e19f7bf9a8e95311b7730952f8daf8f
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-06 06:34:16 +0700

    update src/Person.php - constructor and destructor

--- src/Person.php
+++ src/Person.php
@@ -2,37 +2,21 @@
 
 class Person
 {
-    const string AUTHOR = "anonymous";
-
     public string $name;
+    public string $city;
 
-    public function sayHello(?string $name): string
+    public function __construct(string $name, string $city)
     {
-        return "hello" . ($name ? " $name" : "") . ", my name is $this->name";
+        $this->name = $name;
+        $this->city = $city;
+
+        echo json_encode($this) . "\n";
     }
 
-    public function infoSelf(): string
+    public function __destruct()
     {
-        return self::AUTHOR;
-    }
-
-    public function infoClass(): string
-    {
-        return Person::AUTHOR;
+        echo "object person $this->name destroyed\n";
     }
 }
 
-$person1 = new Person();
-$person2 = new Person();
-
-$person1->name = "john doe";
-$person2->name = "david wilson";
-
-echo Person::AUTHOR . "\n";
-
-echo <<<MULTILINE
-{$person1->sayHello($person2->name)}
-{$person2->sayHello(null)}
-{$person1->infoSelf()}
-{$person2->infoClass()}\n
-MULTILINE;
+$person = new Person("john doe", "surabaya");
