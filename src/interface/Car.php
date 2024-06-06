6fb9cdbea4edece345c9b5b6b9891186744011cb
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-06 12:42:43 +0700

    add src/interface/Car.php - interface

---
+++ src/interface/Car.php
@@ -0,0 +1,62 @@
+<?php
+
+interface HasBrand
+{
+    public function getBrand(): string;
+}
+
+interface IsExpensive
+{
+    public function IsExpensive(): bool;
+}
+
+interface Car extends HasBrand, IsExpensive
+{
+    public function drive(): string;
+
+    public function getTire(): int;
+}
+
+interface IsMaintenance
+{
+    public function isMaintenance(): bool;
+}
+
+class Tesla implements Car, IsMaintenance
+{
+    public function drive(): string
+    {
+        return "drive tesla";
+    }
+
+    public function getTire(): int
+    {
+        return 4;
+    }
+
+    public function getBrand(): string
+    {
+        return "tesla";
+    }
+
+    public function IsExpensive(): bool
+    {
+        return true;
+    }
+
+    public function isMaintenance(): bool
+    {
+        return false;
+    }
+}
+
+$car = new Tesla();
+
+echo <<<MULTILINE
+{$car->drive()}
+{$car->getTire()}
+{$car->getBrand()}\n
+MULTILINE;
+
+echo json_encode($car->isExpensive()) . "\n";
+echo json_encode($car->isMaintenance()) . "\n";
