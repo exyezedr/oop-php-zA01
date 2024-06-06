02af96a70393a526d90c8fe1e347ee9fded1582b
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-06 07:27:27 +0700

    update src/namespace - global namespace

--- src/namespace/conflict.php
+++ src/namespace/conflict.php
@@ -11,3 +11,15 @@ namespace Data\Two {
     {
     }
 }
+
+namespace {
+    require_once __DIR__ . "/helper.php";
+
+    $conflict1 = new \Data\One\Conflict();
+    $conflict2 = new \Data\Two\Conflict();
+
+    echo json_encode($conflict1) . "\n";
+    echo json_encode($conflict2) . "\n";
+    echo Helper\APP . "\n";
+    echo Helper\helpMe() . "\n";
+}
