b7f3f2a1522fc3e14a9358e79e5375f5199738a4
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-06 17:00:42 +0700

    update src/class/Data.php - iterator

--- src/class/Data.php
+++ src/class/Data.php
@@ -1,12 +1,34 @@
 <?php
 
-class Data
+class Data implements IteratorAggregate
 {
     public int $first = 1;
     private int $second = 2;
     protected int $third = 3;
+
+    public function getIterator(): ArrayIterator
+    {
+        return new ArrayIterator(["second" => $this->second, "third" => $this->third]);
+    }
+}
+
+function even(int $max): ArrayIterator
+{
+    $array = [];
+
+    for ($i = 1; $i <= $max; $i++) {
+        if ($i % 2 === 0) {
+            $array[] = $i;
+        }
+    }
+
+    return new ArrayIterator($array);
 }
 
 foreach (new Data() as $key => $number) {
     echo "$key = $number\n";
+}
+
+foreach (even(10) as $number) {
+    echo "$number\n";
 }
