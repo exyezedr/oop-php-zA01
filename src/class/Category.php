7ec6efa35eda674a96689dd6efbd4a0cffc95dbf
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-06 11:06:46 +0700

    add src/class/Category.php - getter and setter (encapsulation)

---
+++ src/class/Category.php
@@ -0,0 +1,42 @@
+<?php
+
+class Category
+{
+    private string $name;
+    private bool $expensive;
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        if (trim($name) !== "") {
+            $this->name = $name;
+        }
+    }
+
+    public function isExpensive(): bool
+    {
+        return $this->expensive;
+    }
+
+    public function setExpensive(bool $expensive): void
+    {
+        $this->expensive = $expensive;
+    }
+}
+
+$category = new Category();
+
+$category->setName("laptop");
+$category->setExpensive(true);
+
+$isExpensive = $category->isExpensive() ? "expensive" : "cheap";
+
+echo "{$category->getName()} | $isExpensive\n";
+
+$category->setName("  ");
+
+echo "{$category->getName()} | $isExpensive\n";
