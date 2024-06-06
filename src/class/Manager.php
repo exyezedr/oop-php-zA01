db919dc5f0d2cf41d9836833e8d7315b6534a53a
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-06 07:58:18 +0700

    update src/class/Manager.php

--- src/class/Manager.php
+++ src/class/Manager.php
@@ -9,3 +9,22 @@ class Manager
         return "hello $name, my name is $this->name (manager)";
     }
 }
+
+class VicePresident extends Manager
+{
+    public function sayHello(string $name): string
+    {
+        return "hello $name, my name is $this->name (vice president)";
+    }
+}
+
+$manager = new Manager();
+$vicePresident = new VicePresident();
+
+$manager->name = "john doe";
+$vicePresident->name = "david wilson";
+
+echo <<<MULTILINE
+{$manager->sayHello($vicePresident->name)}
+{$vicePresident->sayHello($manager->name)}\n
+MULTILINE;
