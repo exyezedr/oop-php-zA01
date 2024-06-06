8e3b04d70336ca3cab8880aed43b0f78fdf71356
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-06 08:07:35 +0700

    update src/class/Manager.php - constructor overriding

--- src/class/Manager.php
+++ src/class/Manager.php
@@ -3,26 +3,39 @@
 class Manager
 {
     public string $name;
+    public string $title;
+
+    public function __construct(string $name, string $title)
+    {
+        $this->name = $name;
+        $this->title = $title;
+    }
 
     public function sayHello(string $name): string
     {
-        return "hello $name, my name is $this->name (manager)";
+        return "hello $name, my name is $this->name ($this->title)";
     }
 }
 
 class VicePresident extends Manager
 {
+    public string $secondTitle;
+
+    public function __construct(string $name, string $title)
+    {
+        parent::__construct($name, "vice president");
+
+        $this->secondTitle = $title;
+    }
+
     public function sayHello(string $name): string
     {
-        return "hello $name, my name is $this->name (vice president)";
+        return "hello $name, my name is $this->name ($this->title and $this->secondTitle)";
     }
 }
 
-$manager = new Manager();
-$vicePresident = new VicePresident();
-
-$manager->name = "john doe";
-$vicePresident->name = "david wilson";
+$manager = new Manager("john doe", "manager");
+$vicePresident = new VicePresident("david wilson", "executive director");
 
 echo <<<MULTILINE
 {$manager->sayHello($vicePresident->name)}
