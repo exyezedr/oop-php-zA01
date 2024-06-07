0f48e77cf6100ad152d49418a97ee3b7b879d5f7
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-07 17:36:46 +0700

    update src/class/Person.php - first class callable

--- src/class/Person.php
+++ src/class/Person.php
@@ -2,12 +2,22 @@
 
 class Person
 {
-    public function __construct(
-        public readonly string $name,
-        public readonly string $city,
-    ) {
-        echo json_encode($this) . "\n";
+    public function __construct(public string $name)
+    {
+    }
+
+    public function sayHello(string $name): string
+    {
+        return "hello $name, my name is $this->name";
     }
 }
 
-$person = new Person("john doe", "surabaya");
+$person = new Person("john doe");
+
+$reference1 = $person->sayHello(...);
+$reference2 = array_sum(...);
+
+echo <<<MULTILINE
+{$reference1("david wilson")}
+{$reference2([10, 10, 10, 10, 10])}
+MULTILINE;
