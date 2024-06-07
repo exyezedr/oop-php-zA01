2137a026b52a15cbbc0aaf88064107b4e029506c
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-07 08:19:50 +0700

    update src/class/Person.php - constructor property promotion

--- src/class/Person.php
+++ src/class/Person.php
@@ -1,45 +1,14 @@
 <?php
 
-trait HasName
-{
-    public string $name;
-}
-
-trait SayHello
-{
-    private function sayHello(?string $name): string
-    {
-        return "hello" . ($name ? " $name" : "") . ", my name is $this->name";
-    }
-}
-
-trait CanRun
-{
-    abstract public function run(): string;
-}
-
-trait All
-{
-    use SayHello, HasName, CanRun {
-        sayHello as public;
-    }
-}
-
 class Person
 {
-    use All;
-
-    public function run(): string
-    {
-        return "$this->name is running";
+    public function __construct(
+        public string $name,
+        public string $city,
+        public string $country
+    ) {
+        echo json_encode($this) . "\n";
     }
 }
 
-$person = new Person();
-
-$person->name = "john doe";
-
-echo <<<MULTILINE
-{$person->sayHello("david wilson")}
-{$person->run()}\n
-MULTILINE;
+$person = new Person("john doe", "surabaya", "indonesia");
