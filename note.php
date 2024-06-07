0bb1f781478ccad4bc9de774af5f8a26fbea52b2
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-07 17:56:37 +0700

    add note.php

---
+++ note.php
@@ -0,0 +1,145 @@
+<?php
+
+/**
+ * 1. ParentPerson, 1.1. Person
+ * 2. Address
+ *      - class and object
+ *      - property and method
+ *      - this, constant, and self
+ *      - constructor and destructor
+ *      - trait
+ *      - trait overriding
+ *      - trait visibility override
+ *      - constructor property promotion
+ *      - null safe operator
+ *      - string able
+ *      - readonly property
+ *      - first class callable
+ *
+ * ==================================================
+ *
+ * 1. Conflict
+ *      - trait conflict
+ *
+ * ==================================================
+ *
+ * 1. Login
+ *      - allow::class on object
+ *
+ * ==================================================
+ *
+ * 1. Student
+ *      - clone
+ *      - comparing object
+ *      - to string, invoke, and debug info
+ *
+ * ==================================================
+ *
+ * 1. Zero
+ *      - overloading
+ *
+ * ==================================================
+ *
+ * 1. Manager, 1.1. VicePresident
+ *      - inheritance
+ *      - method overriding
+ *      - constructor overriding
+ *
+ * ==================================================
+ *
+ * 1. Product, 1.1. ProductDummy
+ *      - visibility
+ *
+ * ==================================================
+ *
+ * 1. Category
+ *      - getter and setter (encapsulation)
+ *
+ * ==================================================
+ *
+ * 1. Shape, 1.1. Rectangle
+ *      - parent
+ *
+ * ==================================================
+ *
+ * 1. Programmer, 1.1. BackendProgrammer, 1.2. FrontendProgrammer
+ * 2. Company
+ *      - polymorphism
+ *      - type check and casting
+ *
+ * ==================================================
+ *
+ * 1. SocialMedia, 1.1. Facebook, 1.1.1. FakeFacebook
+ *      - final
+ *
+ * ==================================================
+ *
+ * 1. MathHelper
+ *      - static
+ *
+ * ==================================================
+ *
+ * 1. LoginRequest
+ * 2. RegisterUserRequest
+ * 3. ValidationException
+ * 4. Attribute NotBlank
+ * 5. Attribute Length
+ *      - exception
+ *      - reflection
+ *
+ * ==================================================
+ *
+ * 1. Data
+ *      - iteration
+ *      - iterator
+ *      - generator
+ *
+ * ==================================================
+ *
+ * 1. Food, 1.1. AnimalFood
+ * 2. abstract Animal, 2.1. Cat, 2.2. Dog
+ * 3. interface AnimalShelter, 3.1. CatShelter, 3.2. DogShelter
+ *      - abstract
+ *      - covariance
+ *      - contra variance
+ *
+ * ==================================================
+ *
+ * 1. interface HasBrand,
+ * 2. interface IsExpensive,
+ *      1.1. , 2.1. interface Car
+ * 3. interface IsMaintenance
+ *      1.1.1. , 2.1.1. , 3.1. Tesla
+ *          - interface
+ *          - pure intersection type
+ *
+ * ==================================================
+ *
+ * interface HelloWorld
+ *      - anonymous class
+ *
+ * ==================================================
+ *
+ * namespace
+ *      - namespace
+ *      - global namespace
+ *      - import
+ *
+ * ==================================================
+ *
+ * 1. interface SayHello, 1.1. enum Gender
+ * 2. Customer
+ *      - enum
+ *      - backed enum
+ *      - enum method
+ *      - enum inheritance and constant
+ *
+ * ==================================================
+ *
+ * 01. standard class
+ * 02. date time
+ * 03. regular expression
+ * 04. union type
+ * 05. mixed type
+ * 06. never return type
+ */
