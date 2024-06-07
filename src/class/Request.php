514c7c17d4cdc274cb71e2f6ca98b3bdf1eda3b6
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-07 08:13:38 +0700

    update src/class/Request.php - reflection

--- src/class/Request.php
+++ src/class/Request.php
@@ -1,50 +1,84 @@
 <?php
 
-class ValidationException extends Exception
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class NotBlank
 {
+}
+
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class Length
+{
+    public ?int $min;
+    public ?int $max;
+
+    public function __construct(int $min, int $max)
+    {
+        $this->min = $min;
+        $this->max = $max;
+    }
 }
 
 class LoginRequest
 {
+    #[Length(min: 4, max: 10)]
+    #[NotBlank]
     public ?string $username;
-    public ?string $password;
-}
 
-class RegisterUserRequest
-{
-    public ?string $name;
-    public ?string $address;
-    public ?string $email;
+    #[Length(min: 8, max: 10)]
+    #[NotBlank]
+    public ?string $password;
 }
 
 function validate(object $request): void
 {
-    $reflection = new ReflectionClass($request);
-    $properties = $reflection->getProperties(ReflectionProperty::IS_PUBLIC);
+    $class = new ReflectionClass($request);
+    $properties = $class->getProperties(ReflectionProperty::IS_PUBLIC);
 
     foreach ($properties as $property) {
+        validateNotBlank($property, $request);
+        validateLength($property, $request);
+    }
+}
+
+function validateNotBlank(ReflectionProperty $property, object $request): void
+{
+    $attributes = $property->getAttributes(NotBlank::class);
+
+    if (!empty($attributes)) {
         if (!$property->isInitialized($request)) {
-            throw new ValidationException("$property->name is not set");
+            throw new Exception("$property->name is not set");
         } else if (is_null($property->getValue($request))) {
-            throw new ValidationException("$property->name is null");
+            throw new Exception("$property->name is null");
         } else if (trim($property->getValue($request)) === "") {
-            throw new ValidationException("$property->name is empty");
+            throw new Exception("$property->name is empty");
+        }
+    }
+}
+
+function validateLength(ReflectionProperty $property, object $request): void
+{
+    $value = $property->getValue($request);
+    $attributes = $property->getAttributes(Length::class);
+
+    foreach ($attributes as $attribute) {
+        $length = $attribute->newInstance();
+        $valueLength = strlen($value);
+
+        if ($valueLength < $length->min) {
+            throw new Exception("$property->name is to short");
+        } else if ($valueLength > $length->max) {
+            throw new Exception("$property->name is to long");
         }
     }
 }
 
 $loginRequest = new LoginRequest();
-$registerUserRequest = new RegisterUserRequest();
 
 $loginRequest->username = "john-doe";
 $loginRequest->password = "jd10";
 
 try {
     validate($loginRequest);
-    echo "valid\n";
-
-    validate($registerUserRequest);
-    echo "valid\n";
-} catch (ValidationException $error) {
-    echo "error = {$error->getMessage()}\n";
+} catch (Exception $exception) {
+    echo "error = {$exception->getMessage()}\n";
 }
